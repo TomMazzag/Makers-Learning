df42474480571fe1842e1a279d1a1d5e562a26b7
Thomas Mazzag <92039929+TomMazzag@users.noreply.github.com>
2023-11-06 15:29:48 +0000

    Extra challenge
    Finished main challenges for this week so looking into some extra learning on swift

---
+++ Week 2/notes.swift
@@ -0,0 +1,43 @@
+func hello() -> String {
+  return "Goodbye, Mars!"
+}
+
+var variableExample = 0
+
+// Notes can be made using //
+
+variableExample = 3
+
+var testInt: testInt
+testInt = 54
+
+print(testInt)
+
+let constantExample = 10
+
+// Addition subtraction multiplacation
+// Standard syntax +, -, *
+
+func exampleFunction(argumentName parameterName: Int) -> String {
+    // Functions in swift have argument labels
+    // Parameter name is used inside the body of the function
+}
+
+//To call the function
+
+exampleFunction(age: 20)
+
+func square(number timesBy: Int) -> Int {
+    return (timesBy * timesBy)
+}
+
+square(number: 5)
+
+// Functions that dont return a value will have a return type of Void
+// Void is the same as ()
+
+// Leave parameter label blank if you want the same name for argument;
+
+func test(data: Int) -> Int {
+    return data
+}
