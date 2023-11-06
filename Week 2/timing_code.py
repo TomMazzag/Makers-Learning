e79774bb052ae28034e097940627b2508c791f93
Thomas Mazzag <92039929+TomMazzag@users.noreply.github.com>
2023-11-06 14:37:10 +0000

    Updated to add correct processes, added addChart

--- Week 2/timing_code.py
+++ Week 2/timing_code.py
@@ -1,6 +1,8 @@
 import random
 import time
+import openpyxl
 from openpyxl import Workbook
+from openpyxl.chart import BarChart,Reference
 
 test_list = []
 for i in range(2500000, 5000000):
@@ -40,7 +42,7 @@ def add_to_chart(process, result):
     sheet[f"A{a}"] = process
     sheet[f"B{b}"] = round(result, 4)
 
-def timeTaken(function):
+def timeTaken(function, description):
     ''' 
     Function is used to work out the cpu execution time
     -- st variable is the start time of the process
@@ -53,11 +55,24 @@ def timeTaken(function):
     et = time.process_time()
     result = et - st
     print('CPU Execution time:', round(result, 4), 'seconds')
-    add_to_chart('Last', result)
+    add_to_chart(description, result)
 
-timeTaken(l)
-timeTaken(r)
-timeTaken(sh)
-timeTaken(so)
+timeTaken(l, 'Last')
+timeTaken(r, 'Reverse')
+timeTaken(sh, 'Shuffle')
+timeTaken(so, 'Sort')
+
+def addChart():
+    chart = BarChart()
+    processes = Reference(sheet, min_col=1, min_row=3, max_col=1, max_row=7)
+    times = Reference(sheet, min_col=2, min_row=3, max_col=2, max_row=7)
+    chart.add_data(times)
+    chart.set_categories(processes)
+    chart.title = f'Time taken for {test_length:,} items'
+    chart.x_axis.title = 'Process'
+    chart.y_axis.title = 'Time (s)'
+    sheet.add_chart(chart, 'E2')
+
+addChart()
 
 workbook.save(filename="Results.xlsx")
